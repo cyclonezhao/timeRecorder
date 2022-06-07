785fd9c3c51414300d7f95db612f616a98cda004
cyclonezhao <lightseeker@qq.com>
2022-06-07 12:16:18 +0800

    识别出没录入事件的空日期

--- timeRecorder.py
+++ timeRecorder.py
@@ -3,6 +3,7 @@ from itertools import groupby
 from builtins import map
 from collections import Counter
 import json
+import datetime
 
 # 读取 timeRecordData.txt 文件
 fileinput = open("timeRecordData.txt", "rt", encoding='utf-8')
@@ -34,20 +35,39 @@ for rawData in rawDataLst:
 		continue;
 	rowData = rawData.split("\t")
 	_lst.append(rowData)
-# 按日期分组，转换eventLst
-_grp = groupby(_lst, lambda v: v[0])
-_lst = []
-for key,value in _grp:
-	# endTime 必须以数字开头
-	value = list(filter(lambda v: v[2][0].isdigit(), value))
-	# beginTime 大于6点
-	value = list(filter(lambda v: int(v[1].split(":")[0]) >= 6, value))
-	eventLst = list(map(lambda v: {"beginTime":v[1],"endTime":v[2],"eventType":v[3],"desc":v[4]}, value))
 	
-	for event in value:
-		eventTypeSet.add(event[3])
+# 生成日期序列，用于识别出没录入事件的空日期
+dateLst = []
+dt1 = datetime.datetime.strptime(_lst[0][0], '%Y年%m月%d日')
+dateLst.append(dt1.strftime("%Y-%m-%d"))
+now = datetime.datetime.now()
+# 两个日期对象差多少天
+diffDay = now - dt1
+diffDay = diffDay.days
+for i in range(diffDay):
+	dt2 = (dt1 + datetime.timedelta(days=i+1))
+	dateLst.append(dt2.strftime("%Y-%m-%d"))
+	
+# 按日期分组，转换eventLst
+_grp = {}
+for v in _lst:
+	_grp.setdefault(datetime.datetime.strptime(v[0], '%Y年%m月%d日').strftime("%Y-%m-%d"), []).append(v)
+
+_lst = []
+for date in dateLst:
+	eventLst = []
+	if date in _grp.keys():
+		value = _grp[date]
+		# endTime 必须以数字开头
+		value = list(filter(lambda v: v[2][0].isdigit(), value))
+		# beginTime 大于6点
+		value = list(filter(lambda v: int(v[1].split(":")[0]) >= 6, value))
+		eventLst = list(map(lambda v: {"beginTime":v[1],"endTime":v[2],"eventType":v[3],"desc":v[4]}, value))
 		
-	_lst.append({"date": key, "eventLst": eventLst})
+		for event in value:
+			eventTypeSet.add(event[3])
+		
+	_lst.append({"date": date, "eventLst": eventLst})
 # 按日期排序
 _lst = sorted(_lst, key=lambda v: v['date'])
 # 转json字符串
